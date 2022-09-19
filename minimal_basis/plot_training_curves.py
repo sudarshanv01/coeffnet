62f657f29a1c40b21b2499d2ba4ee80b6c52386d
Sudarshan Vijay <svijay@lbl.gov>
2022-09-19 10:13:24 -0700

    Have multiple training curves plotted simultaneously

--- minimal_basis/plot_training_curves.py
+++ minimal_basis/plot_training_curves.py
@@ -1,4 +1,5 @@
 """Plot training curves from a log file."""
+from cmath import log
 import os
 import argparse
 
@@ -12,26 +13,33 @@ get_plot_params()
 if __name__ == "__main__":
     """For a gives training log file, plot the training curves."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("log_file", type=str, help="Path to the log file.")
+    parser.add_argument("log_files", nargs="+", type=str, help="Path to the log file.")
     parser.add_argument("--save", type=str, help="Path to save the plot.")
     args = parser.parse_args()
 
     if args.save is None:
-        logfilename = os.path.basename(args.log_file)
-        savefile = os.path.join("plots", logfilename.replace(".log", ".png"))
+        logfilename = [
+            os.path.basename(lfile).replace(".log", "") for lfile in args.log_files
+        ]
+        logfilename = "_".join(logfilename) + ".png"
+        savefile = os.path.join("plots", logfilename)
 
     # Read the log file.
-    log_data = np.loadtxt(args.log_file, delimiter="\t", skiprows=1)
+    log_data = []
+    for log_file in args.log_files:
+        log_data_i = np.loadtxt(log_file, delimiter="\t", skiprows=1)
+        log_data.extend(log_data_i)
+    log_data = np.array(log_data)
 
     # The first column is the epoch number, the second is the
     # loss and the third is the accuracy.
 
     # Plot the loss and accuracy curves.
     fig, ax = plt.subplots(1, 2, figsize=(6, 3), sharex=True, constrained_layout=True)
-    ax[0].plot(log_data[:, 0], log_data[:, 1])
+    ax[0].plot(log_data[:, 1])
     ax[0].set_xlabel("Epoch")
     ax[0].set_ylabel("RMSE Loss (eV)")
-    ax[1].plot(log_data[:, 0], log_data[:, 2])
+    ax[1].plot(log_data[:, 2])
     ax[1].set_xlabel("Epoch")
     ax[1].set_ylabel("Mean absolute error (eV)")
     fig.savefig(savefile, dpi=300)
