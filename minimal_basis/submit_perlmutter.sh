e1c9a449865deb401ab108f61bfaa17ee1136c6d
Sudarshan Vijay <svijay@lbl.gov>
2022-09-22 13:46:39 -0700

    preparing for lawrencium

---
+++ minimal_basis/submit_perlmutter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=job
+#SBATCH --time=0-10:00:00  
+#SBATCH --ntasks=1      
+#SBATCH --cpus-per-task=2     # 2 cpus for the job
+#SBATCH --gpus=1              # 1 gpu for the job 
+#SBATCH --constraint=gpu
+#SBATCH --qos=regular         # use the `regular` queue
+#SBATCH --account=jcesr_g    # don't forget the `_g`; you may want to use `jcesr_g`
+
+module load pytorch/1.11.0
+module load python
+conda activate molml
+
+python model.py 
