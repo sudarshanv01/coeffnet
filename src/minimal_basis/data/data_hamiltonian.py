f6337f6add03a02b503c997c26ac08e4c8d3f81f
Sudarshan Vijay <svijay@lbl.gov>
2023-03-07 17:42:33 -0800

    Basis set for coeff parser

--- src/minimal_basis/data/data_hamiltonian.py
+++ src/minimal_basis/data/data_hamiltonian.py
@@ -1,7 +1,10 @@
-from typing import List, Dict, Union
+from typing import List, Dict, Union, Any
+import logging
 
 import numpy.typing as npt
 import numpy as np
+
+from ase import data as ase_data
 
 import torch
 from torch_geometric.data import Data
@@ -17,6 +20,8 @@ from minimal_basis.data._dtype import (
     TORCH_FLOATS,
     TORCH_INTS,
 )
+
+logger = logging.getLogger(__name__)
 
 
 def convert_to_tensor(
@@ -44,7 +49,7 @@ class HamiltonianDataPoint(Data):
         y: Union[npt.ArrayLike, List[float]] = None,
         edge_index_interpolated_TS: Union[npt.ArrayLike, List[int]] = None,
         pos_interpolated_TS: Union[npt.ArrayLike, List[float]] = None,
-        **kwargs
+        **kwargs,
     ):
         if pos is not None:
             pos_initial_state, pos_final_state = (
@@ -126,8 +131,115 @@ class HamiltonianDataPoint(Data):
             global_attr_final_state=global_attr_final_state,
             y=y,
             pos_interpolated_TS=pos_interpolated_TS,
-            **kwargs
+            **kwargs,
         )
+
+
+class CoefficientMatrixToAtoms:
+    def __init__(
+        self,
+        molecule_graph: MoleculeGraph,
+        basis_info_raw: Dict[str, Any],
+        coefficient_matrix: npt.ArrayLike,
+        store_idx_only: int = None,
+        **kwargs,
+    ):
+        """Store the coefficient matrix and provides some utilities manipulate it."""
+        if store_idx_only:
+            self.coefficient_matrix = coefficient_matrix[:, store_idx_only]
+            self.coefficient_matrix = self.coefficient_matrix[:, np.newaxis, :]
+        else:
+            self.coefficient_matrix = coefficient_matrix
+
+        self.molecule_graph = molecule_graph
+        self.basis_info_raw = basis_info_raw
+
+    def parse_basis_data(self):
+        """Parse the basis information from data from basissetexchange.org
+        json format to a dict containing the number of s, p and d functions
+        for each atom. The resulting dictionary, self.basis_info contains the
+        total set of basis functions for each atom.
+        """
+        # Create a new dict with the basis information
+        logger.info(f"Parsing basis information from {self.basis_file}")
+
+        self.basis_info = {}
+
+        for atom_number in self.basis_info_raw["elements"]:
+            angular_momentum_all = []
+            for basis_index, basis_functions in enumerate(
+                self.basis_info_raw[atom_number]["electron_shells"]
+            ):
+                angular_momentum_all.extend(basis_functions["angular_momentum"])
+            angular_momentum_all = [
+                self.n_to_l_basis[element] for element in angular_momentum_all
+            ]
+            self.basis_info[int(atom_number)] = angular_momentum_all
+
+    def _get_atomic_number(cls, symbol):
+        """Get the atomic number of an element based on the symbol."""
+        return ase_data.atomic_numbers[symbol]
+
+    def get_basis_index(self, molecule):
+        """Get the basis index for each atom in the atom in the molecule."""
+
+        # Basis information
+        basis_s = []
+        basis_p = []
+        basis_d = []
+
+        # Store the grouping of the basis index of each atom in the molecule
+        basis_atom = []
+        irreps_all_atom = []
+
+        tot_basis_idx = 0
+        for atom_name in molecule.species:
+
+            # Store the irreps
+            irreps_atom = ""
+
+            # Get the basis functions for this atom
+            atomic_number = self._get_atomic_number(atom_name.symbol)
+            basis_functions = self.basis_info[atomic_number]
+
+            # Get the atom specific list
+            basis_s_ = []
+            basis_p_ = []
+            basis_d_ = []
+
+            # Store the initial basis functions
+            counter_tot_basis_idx = tot_basis_idx
+
+            for basis_function in basis_functions:
+                if basis_function == "s":
+                    range_idx = list(range(tot_basis_idx, tot_basis_idx + 1))
+                    basis_s_.append(range_idx)
+                    tot_basis_idx += 1
+                    irreps_atom += "+1x0e"
+                elif basis_function == "p":
+                    range_idx = list(range(tot_basis_idx, tot_basis_idx + 3))
+                    basis_p_.append(range_idx)
+                    tot_basis_idx += 3
+                    irreps_atom += "+1x1o"
+                elif basis_function == "d":
+                    range_idx = list(range(tot_basis_idx, tot_basis_idx + 5))
+                    basis_d_.append(range_idx)
+                    tot_basis_idx += 5
+                    irreps_atom += "+1x2e"
+
+            irreps_atom = irreps_atom[1:]
+            irreps_all_atom.append(irreps_atom)
+
+            # Store the basis index for this atom
+            basis_atom.append(list(range(counter_tot_basis_idx, tot_basis_idx)))
+
+            # Store the s,p and d basis functions for this atom
+            basis_s.append(basis_s_)
+            basis_p.append(basis_p_)
+            basis_d.append(basis_d_)
+
+        # Split the Hamilonian into node and edge features
+        all_basis_idx = [basis_s, basis_p, basis_d]
 
 
 class MatrixSplitAtoms:
@@ -136,7 +248,7 @@ class MatrixSplitAtoms:
         matrix: npt.ArrayLike,
         molecule_graph: MoleculeGraph,
         basis_info_atom: Dict,
-        **kwargs
+        **kwargs,
     ):
         """Split an atomic matrix into node and edge features."""
 
