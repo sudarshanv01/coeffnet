2e98a28686e6ac3b9f6a874bad1826e751ad3496
Sudarshan Vijay <svijay@lbl.gov>
2023-03-15 12:25:17 -0700

    Class for orthogonalising qchem inputs

---
+++ src/minimal_basis/predata/predata_qchem.py
@@ -0,0 +1,84 @@
+from typing import List, Tuple, Union
+
+import numpy.typing as npt
+import numpy as np
+
+
+class BaseQuantitiesQChem:
+    def __init__(
+        self,
+        fock_matrix: npt.ArrayLike,
+        eigenvalues: npt.ArrayLike,
+        coeff_matrix: npt.ArrayLike,
+    ):
+        """QChem provides the Fock matrix, the eigenvalues and the
+        coeffient matrix. The other quantities in the current basis
+        and orthogonal form are provided by the methods in this class.
+
+        Args:
+            fock_matrix (npt.ArrayLike): The Fock matrix.
+            eigenvalues (npt.ArrayLike): The eigenvalues.
+            coeff_matrix (npt.ArrayLike): The coefficient matrix.
+
+        """
+
+        self.fock_matrix = np.array(fock_matrix)
+        self.eigenvalues = np.array(eigenvalues)
+        self.coeff_matrix = np.array(coeff_matrix)
+
+        self.overlap_matrix = None
+        self.orthogonalisation_matrix = None
+        self.orthogonal_fock_matrix = None
+        self.ortho_coeff_matrix = None
+
+        if len(self.eigenvalues.shape) == 1:
+            self.diagonalised_eigen = np.zeros(
+                (len(self.eigenvalues), len(self.eigenvalues))
+            )
+            np.fill_diagonal(self.diagonalised_eigen, self.eigenvalues)
+
+    def get_overlap_matrix(self) -> npt.ArrayLike:
+        self.generate_overlap_matrix()
+        return self.overlap_matrix
+
+    def get_orthogonalization_matrix(self) -> npt.ArrayLike:
+        self.generate_orthogonalization_matrix()
+        return self.orthogonalisation_matrix
+
+    def get_orthogonal_fock_matrix(self) -> npt.ArrayLike:
+        self.generate_orthogonal_fock_matrix()
+        return self.orthogonal_fock_matrix
+
+    def get_ortho_coeff_matrix(self) -> npt.ArrayLike:
+        self.generate_orthogonal_fock_matrix()
+        self.generate_orthogonal_coeff_matrix()
+        return self.ortho_coeff_matrix
+
+    def generate_overlap_matrix(self) -> None:
+        """Determine the overlap element from the Hamiltonian, the
+        eigen energies and the cofficient matrix."""
+        self.overlap_matrix = np.dot(
+            np.dot(self.fock_matrix, self.coeff_matrix),
+            np.linalg.inv(np.dot(self.coeff_matrix, self.diagonalised_eigen)),
+        )
+
+    def generate_orthogonalization_matrix(self):
+        """Determine the orthogonalization matrix from the overlap matrix."""
+        eigenval_overlap, eigenvec_overlap = np.linalg.eigh(self.overlap_matrix)
+        self.orthogonalisation_matrix = np.dot(
+            eigenvec_overlap,
+            np.dot(np.diag(1 / np.sqrt(eigenval_overlap)), eigenvec_overlap.T),
+        )
+
+    def generate_orthogonal_fock_matrix(self):
+        self.orthogonal_fock_matrix = np.dot(
+            np.dot(self.orthogonalisation_matrix.T, self.fock_matrix),
+            self.orthogonalisation_matrix,
+        )
+
+    def generate_orthogonal_coeff_matrix(self):
+        """Diagonalize the Fock matrix."""
+        eigenval_ortho_fock, eigenvec_ortho_fock = np.linalg.eigh(
+            self.orthogonal_fock_matrix
+        )
+        self.ortho_coeff_matrix = eigenvec_ortho_fock
