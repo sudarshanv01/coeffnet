514242c51d637023c17110e9a76c72911144639b
Sudarshan Vijay <svijay@lbl.gov>
2023-04-28 05:14:50 -0700

    Calculate quantities for grambow-green dataset

---
+++ datagen/grambow_green_dataset/calculate_quantities.py
@@ -0,0 +1,81 @@
+import logging
+import yaml
+import copy
+import argparse
+
+from bson.objectid import ObjectId
+
+from pymatgen.core import Molecule
+
+from atomate.qchem.fireworks.core import SinglePointFW, ForceFW
+from atomate.common.powerups import add_tags
+
+from fireworks import LaunchPad, Workflow
+
+from instance_mongodb import instance_mongodb_sei
+
+from fireworks.user_objects.dupefinders.dupefinder_exact import DupeFinderExact
+
+lp = LaunchPad.from_file("/global/u1/s/svijay/fw_config/my_launchpad_mlts.yaml")
+
+
+def get_cli():
+    args = argparse.ArgumentParser()
+    args.add_argument("--dryrun", action="store_true", default=False)
+    return args.parse_args()
+
+
+if __name__ == "__main__":
+    """Run calculations for perturbed structures."""
+
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
+
+    db = instance_mongodb_sei(project="mlts")
+
+    args = get_cli()
+    logger.info("Command line arguments: {}".format(args))
+
+    collection = db.grambow_green_calculation
+    initial_structure_collection = db.grambow_green_initial_structures
+    find_tags = {"functional": "b97d3"}
+
+    with open("config/reproduce_paper_parameters.yaml", "r") as f:
+        params = yaml.safe_load(f)
+    nbo_params = {"nbo_params": {"version": 7}}
+    params.update(nbo_params)
+
+    count_structures = 0
+    for document in initial_structure_collection.find(find_tags):
+
+        for state in ["reactant", "transition_state", "product"]:
+            molecule = document[state]
+            molecule = Molecule.from_dict(molecule)
+
+            tags = {
+                "functional": find_tags["functional"],
+                "state": state,
+                "quantities": ["nbo", "coeff_matrix"],
+            }
+            print(tags)
+
+            if collection.count_documents({"tags": tags}) > 0:
+                logger.info(f"Skipping {tags['label']}")
+                continue
+
+            count_structures += 1
+
+            firew = SinglePointFW(
+                molecule=molecule,
+                qchem_input_params=params,
+                extra_scf_print=True,
+                db_file=">>db_file<<",
+            )
+
+            wf = Workflow([firew])
+            wf = add_tags(wf, tags)
+
+            if not args.dryrun:
+                lp.add_wf(wf)
+
+        logger.info(f"{count_structures} structures processed for calculation.")
--- datagen/grambow_green_dataset/visualise_data.py
+++ datagen/grambow_green_dataset/visualise_data.py
@@ -5,8 +5,13 @@ from ase import io as ase_io
 from pymatgen.io.ase import AseAtomsAdaptor
 from pymatgen.io.qchem.outputs import QCOutput
 
+from instance_mongodb import instance_mongodb_sei
+
 if __name__ == "__main__":
     """Store all data in representative xyz files for easy visualisation."""
+
+    db = instance_mongodb_sei("mlts")
+    collection = db.grambow_green_initial_structures
 
     rxn_folders = glob.glob("data/b97d3/rxn*")
 
@@ -17,7 +22,9 @@ if __name__ == "__main__":
         for state in ["r", "ts", "p"]:
             try:
                 qc_output = QCOutput.multiple_outputs_from_file(
-                    f"{rxn_folder}/{state}{rxn_number}.log", keep_sub_files=False
+                    QCOutput,
+                    f"{rxn_folder}/{state}{rxn_number}.log",
+                    keep_sub_files=False,
                 )
                 if state in ["r", "ts"]:
                     idx = 1
@@ -25,10 +32,26 @@ if __name__ == "__main__":
                     idx = 0
                 atoms = qc_output[idx].data["molecule_from_optimized_geometry"]
 
-                ase_atoms = AseAtomsAdaptor.get_atoms(atoms)
-                molecules.append(ase_atoms)
+                # ase_atoms = AseAtomsAdaptor.get_atoms(atoms)
+                molecules.append(atoms)
             except (KeyError, IndexError):
                 print(f"No {state} for {rxn_number}")
                 continue
 
-        ase_io.write(f"data/b97d3/xyz/{rxn_number}.xyz", molecules)
+        if len(molecules) == 3:
+            ase_molecules = [
+                AseAtomsAdaptor.get_atoms(molecule) for molecule in molecules
+            ]
+            ase_io.write(f"data/b97d3/xyz/{rxn_number}.xyz", ase_molecules)
+
+            collection.insert_one(
+                {
+                    "rxn_number": rxn_number,
+                    "reactant": molecules[0].as_dict(),
+                    "transition_state": molecules[1].as_dict(),
+                    "product": molecules[2].as_dict(),
+                    "functional": "b97d3",
+                }
+            )
+
+            print(f"Stored xyz file and data in MongoDB for {rxn_number}")
