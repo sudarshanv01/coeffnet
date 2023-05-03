795a3a7557ffc77537972754972a62016ddef9ec
Sudarshan Vijay <svijay@lbl.gov>
2023-05-03 10:23:58 -0700

    Visualize the endstate structrues

---
+++ datagen/sn2_dataset/store_endstate_data.py
@@ -0,0 +1,84 @@
+import logging
+
+from typing import List, Dict, Any
+
+import os
+
+from pymatgen.core.structure import Molecule
+from pymatgen.analysis.graphs import MoleculeGraph
+from pymatgen.analysis.local_env import OpenBabelNN
+from pymatgen.io.ase import AseAtomsAdaptor
+
+from ase import io as ase_io
+
+from instance_mongodb import instance_mongodb_sei
+
+
+def expected_endstate(endstate_molecule: Molecule, initial_molecule: Molecule) -> bool:
+    """Check if the endstate is the one we are looking for."""
+
+    initial_molecule_graph = MoleculeGraph.with_local_env_strategy(
+        initial_molecule, OpenBabelNN()
+    )
+    molecule_graph = MoleculeGraph.with_local_env_strategy(
+        endstate_molecule, OpenBabelNN()
+    )
+
+    if initial_molecule_graph.isomorphic_to(molecule_graph):
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    """Looking at the calculation, store endstate data."""
+
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
+
+    os.makedirs("output/visualise_endstates", exist_ok=True)
+
+    db = instance_mongodb_sei(project="mlts")
+    data_collection = db.rudorff_lilienfeld_data
+    calculation_collection = db.rudorff_lilienfeld_calculation
+
+    cursor = calculation_collection.find(
+        {"task_label": "frequency flattening structure optimization"},
+        {
+            "calcs_reversed": 1,
+            "tags": 1,
+            "output": 1,
+            "structure_change": 1,
+        },
+    )
+
+    for doc in cursor:
+        state = doc["tags"]["state"]
+        rxn_number = doc["tags"]["rxn_number"]
+        reaction_name = doc["tags"]["reaction_name"]
+
+        molecule = doc["output"]["optimized_molecule"]
+        molecule = Molecule.from_dict(molecule)
+
+        initial_molecule = doc["calcs_reversed"][-1]["initial_molecule"]
+        initial_molecule = Molecule.from_dict(initial_molecule)
+
+        if not expected_endstate(molecule, initial_molecule):
+            continue
+
+        ase_atoms = [AseAtomsAdaptor.get_atoms(molecule)]
+        ase_atoms += [AseAtomsAdaptor.get_atoms(initial_molecule)]
+
+        ase_io.write(
+            "output/visualise_endstates/{}_{}_{}.xyz".format(
+                state, rxn_number, reaction_name
+            ),
+            ase_atoms,
+        )
+
+        data_collection.update_one(
+            {"rxn_number": rxn_number, "reaction_name": reaction_name},
+            {"$set": {f"{state}_molecule": molecule.as_dict()}},
+            upsert=True,
+        )
+        logger.info("Updated {} {} {}".format(state, rxn_number, reaction_name))
---
+++ datagen/sn2_dataset/visualise_states_reaction.py
@@ -0,0 +1,74 @@
+from typing import List
+
+import logging
+
+import os
+
+import copy
+
+import numpy as np
+
+from pymatgen.core.structure import Molecule
+from pymatgen.io.ase import AseAtomsAdaptor
+
+import ase.io as ase_io
+
+from instance_mongodb import instance_mongodb_sei
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+if __name__ == "__main__":
+    """Visualize the initial, transition and final states of the reaction."""
+
+    if not os.path.exists("output/visualise_states"):
+        os.makedirs("output/visualise_states")
+
+    db = instance_mongodb_sei(project="mlts")
+    collection = db.rudorff_lilienfeld_data
+
+    for doc in collection.find({}):
+        transition_state_molecule = doc["transition_state_molecule"]
+        transition_state_molecule = Molecule.from_dict(transition_state_molecule)
+
+        rxn_number = doc["rxn_number"]
+        reaction_name = doc["reaction_name"]
+
+        perturbed_molecule_keys = [
+            key for key in doc.keys() if "perturbed_molecule" in key
+        ]
+        conditional_accept = (
+            len(perturbed_molecule_keys) > 0
+            and len([key for key in perturbed_molecule_keys if "-" in key]) > 0
+            and len([key for key in perturbed_molecule_keys if "-" not in key]) > 0
+        )
+        if not conditional_accept:
+            continue
+
+        positive_perturbed_molecules = [
+            doc[key] for key in perturbed_molecule_keys if "-" not in key
+        ]
+
+        negative_perturbed_molecules = [
+            doc[key] for key in perturbed_molecule_keys if "-" in key
+        ]
+
+        initial_state_molecule = positive_perturbed_molecules[0]
+        initial_state_molecule = Molecule.from_dict(initial_state_molecule)
+
+        final_state_molecule = negative_perturbed_molecules[0]
+        final_state_molecule = Molecule.from_dict(final_state_molecule)
+
+        initial_state_atoms = AseAtomsAdaptor.get_atoms(initial_state_molecule)
+        final_state_atoms = AseAtomsAdaptor.get_atoms(final_state_molecule)
+        transition_state_atoms = AseAtomsAdaptor.get_atoms(transition_state_molecule)
+        write_list = [
+            initial_state_atoms,
+            transition_state_atoms,
+            final_state_atoms,
+        ]
+
+        ase_io.write(
+            "output/visualise_states/{}_{}.xyz".format(rxn_number, reaction_name),
+            write_list,
+        )
