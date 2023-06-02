3e169c49d4dfc87b15a03427fec374fd7788f71f
Sudarshan Vijay <svijay@lbl.gov>
2023-06-02 10:33:41 -0700

    Store only relevant info in creating loss

--- src/minimal_basis/loss/eigenvectors.py
+++ src/minimal_basis/loss/eigenvectors.py
@@ -2,6 +2,8 @@ import itertools
 
 import torch
 
+from torch.nn import functional as F
+
 from torch import nn
 
 
@@ -9,7 +11,7 @@ class Unsigned_MSELoss(nn.Module):
     def __init__(self):
         super(Unsigned_MSELoss, self).__init__()
 
-    def forward(self, input, target, batch, batch_size):
+    def forward(self, input, target, batch, batch_size, reduction="sum"):
         """Since eigenvectors are accurate only upto a sign change, we need to
         make sure that the loss is calculated correctly. This function takes
         care of that for multiple graphs in batch.
@@ -27,7 +29,11 @@ class Unsigned_MSELoss(nn.Module):
         signed = signs[:, batch]
         signed = signed.reshape(-1, signed.shape[-1], 1)
         signed_input = input * signed
-        loss = torch.sum((signed_input - target) ** 2, dim=(1, 2))
-        loss = torch.min(loss)
+
+        _loss = torch.sum((signed_input - target) ** 2, dim=(1, 2))
+        arg_loss = torch.argmin(_loss)
+
+        true_signed_input = input * signed[arg_loss]
+        loss = F.mse_loss(true_signed_input, target, reduction=reduction)
 
         return loss
