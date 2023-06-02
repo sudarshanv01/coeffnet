1b37ef5998bc6fe7c91657f07bf8395d72ebd7ad
Sudarshan Vijay <svijay@lbl.gov>
2023-06-02 10:54:00 -0700

    Determine the best signs outside loss function

--- src/minimal_basis/loss/eigenvectors.py
+++ src/minimal_basis/loss/eigenvectors.py
@@ -1,5 +1,7 @@
 import itertools
 
+import numpy as np
+
 import torch
 
 from torch.nn import functional as F
@@ -10,6 +12,21 @@ from torch import nn
 class Unsigned_MSELoss(nn.Module):
     def __init__(self):
         super(Unsigned_MSELoss, self).__init__()
+
+    @staticmethod
+    def determine_best_sign(_input, _target, batch, batch_size):
+        """Determine the best sign combination for the input."""
+        sign_combinations = itertools.product([1, -1], repeat=batch_size)
+        sign_combinations = np.array(list(sign_combinations))
+        signed = sign_combinations[:, batch]
+
+        signed = signed.reshape(-1, signed.shape[-1], 1)
+        signed_input = _input * signed
+
+        combined_loss = np.sum((signed_input - _target) ** 2, axis=(1, 2))
+        arg_loss = np.argmin(combined_loss)
+
+        return signed[arg_loss]
 
     def forward(self, input, target, batch, batch_size, reduction="sum"):
         """Since eigenvectors are accurate only upto a sign change, we need to
@@ -22,18 +39,11 @@ class Unsigned_MSELoss(nn.Module):
             batch (int): The index of the graph in the batch.
             batch_size (int): The size of the batch.
         """
-
-        sign_combinations = itertools.product([1, -1], repeat=batch_size)
-        sign_combinations = list(sign_combinations)
-        signs = torch.tensor(sign_combinations, dtype=input.dtype, device=input.device)
-        signed = signs[:, batch]
-        signed = signed.reshape(-1, signed.shape[-1], 1)
-        signed_input = input * signed
-
-        _loss = torch.sum((signed_input - target) ** 2, dim=(1, 2))
-        arg_loss = torch.argmin(_loss)
-
-        true_signed_input = input * signed[arg_loss]
+        _input = input.detach().cpu().numpy()
+        _target = target.detach().cpu().numpy()
+        signs = self.determine_best_sign(_input, _target, batch, batch_size)
+        signs = torch.tensor(signs, dtype=torch.float32, device=input.device)
+        true_signed_input = input * signs
         loss = F.mse_loss(true_signed_input, target, reduction=reduction)
 
         return loss
