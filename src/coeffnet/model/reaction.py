Fix normalization in model when running with multiple eigenvalues

src/coeffnet/model/reaction.py
@@ -1,30 +1,27 @@
-from typing import Union, Dict, Optional
-
 import logging
+from typing import Dict, Optional, Union
 
 import torch
-from torch_geometric.data import Data
-from torch_scatter import scatter
-
-from torch_cluster import radius_graph
-
 from e3nn import o3
 from e3nn.math import soft_one_hot_linspace
 from e3nn.nn.models.gate_points_2102 import Network as GateNetwork
 from e3nn.nn.models.gate_points_2102 import smooth_cutoff
 from e3nn.nn.models.v2106.gate_points_networks import MessagePassing
+from torch_cluster import radius_graph
+from torch_geometric.data import Data
+from torch_scatter import scatter
 
 logger = logging.getLogger(__name__)
 
 
-def normalize_to_sum_squares_one(x: torch.Tensor, batch: torch.Tensor) -> torch.Tensor:
-    """Normalize the output such that the sum of squares of each graph is 1."""
-
+def normalize_to_sum_squares_one(
+    x: torch.Tensor, batch: torch.Tensor, number_eigenvalues: int = 1
+) -> torch.Tensor:
+    """Normalize the output such that the sum of squares of each graph is 1 per eigenvalue."""
     sum_squares_output = torch.sum(x**2, dim=1)
     sum_squares_graph = scatter(sum_squares_output, batch, dim=0, reduce="sum")
     normalization_factor = torch.sqrt(sum_squares_graph)
-    x = x / normalization_factor[batch].unsqueeze(1)
-
+    x = torch.sqrt(number_eigenvalues) * x / normalization_factor[batch].unsqueeze(1)
     return x
 
 
@@ -252,7 +249,9 @@ class GateReactionModel(torch.nn.Module):
             )
         if self.normalize_sumsq:
             output_network_initial_state = normalize_to_sum_squares_one(
-                output_network_initial_state, data.batch
+                output_network_initial_state,
+                data.batch,
+                data.number_eigenvalues,
             )
 
         kwargs_final_state = {
@@ -270,7 +269,9 @@ class GateReactionModel(torch.nn.Module):
             output_network_final_state = output_network_final_state * data.basis_mask
         if self.normalize_sumsq:
             output_network_final_state = normalize_to_sum_squares_one(
-                output_network_final_state, data.batch
+                output_network_final_state,
+                data.batch,
+                data.number_eigenvalues,
             )
 
         p = data.p[0]
@@ -300,7 +301,9 @@ class GateReactionModel(torch.nn.Module):
             )
         if self.normalize_sumsq:
             output_network_interpolated_transition_state = normalize_to_sum_squares_one(
-                output_network_interpolated_transition_state, data.batch
+                output_network_interpolated_transition_state,
+                data.batch,
+                data.number_eigenvalues,
             )
         if self.reduce_output:
             output_network_interpolated_transition_state = scatter(
@@ -567,7 +570,9 @@ class MessagePassingReactionModel(torch.nn.Module):
             )
         if self.normalize_sumsq:
             output_network_initial_state = normalize_to_sum_squares_one(
-                output_network_initial_state, data.batch
+                output_network_initial_state,
+                data.batch,
+                data.number_eigenvalues,
             )
 
         output_network_final_state = self.network_final_state(kwargs_final_state)
@@ -576,7 +581,9 @@ class MessagePassingReactionModel(torch.nn.Module):
             output_network_final_state = output_network_final_state * data.basis_mask
         if self.normalize_sumsq:
             output_network_final_state = normalize_to_sum_squares_one(
-                output_network_final_state, data.batch
+                output_network_final_state,
+                data.batch,
+                data.number_eigenvalues,
             )
 
         p = data.p[0]
@@ -606,7 +613,9 @@ class MessagePassingReactionModel(torch.nn.Module):
             )
         if self.normalize_sumsq:
             output_network_interpolated_transition_state = normalize_to_sum_squares_one(
-                output_network_interpolated_transition_state, data.batch
+                output_network_interpolated_transition_state,
+                data.batch,
+                data.number_eigenvalues,
             )
         if self.reduce_output:
             output_network_interpolated_transition_state = scatter(

tests/test_model.py
@@ -90,22 +90,33 @@ def test_sum_square_one_minimal_basis(dataset_options_factory, model_options_fac
         assert torch.allclose(sum_sq_output, torch.ones_like(sum_sq_output))
 
 
-def test_sum_square_one_full_basis(dataset_options_factory, model_options_factory):
-    """Test that the output of each network pass is normalized to sum of squares one."""
-    dataset_options = dataset_options_factory(basis_type="full")
+@pytest.mark.parametrize("basis_type", ["full", "minimal"])
+@pytest.mark.parametrize("idx_eigenvalue", [[0], [0, -1]])
+def test_normalized_sum_squared_coefficients(
+    dataset_options_factory, model_options_factory, basis_type, idx_eigenvalue
+):
+    """Test that the output of each network pass is normalized to the right number."""
+    dataset_options = dataset_options_factory(
+        basis_type=basis_type, idx_eigenvalue=idx_eigenvalue
+    )
     dataset = ReactionDataset(**dataset_options)
     loader = DataLoader(dataset, batch_size=1, shuffle=False)
-
     model_options = model_options_factory(
-        prediction_mode="coeff_matrix", basis_type="full"
+        prediction_mode="coeff_matrix",
+        basis_type=basis_type,
     )
     model_options["normalize_sumsq"] = True
+    # Overwrite the irreps of the model with that coming from the dataset
+    model_options["irreps_in"] = dataset.irreps_in
+    model_options["irreps_out"] = dataset.irreps_out
+    number_eigenvals = len(idx_eigenvalue)
     model = GateReactionModel(**model_options)
-
     for batch in loader:
         output = model(batch)
         sum_sq_output = torch.sum(output**2)
-        assert torch.allclose(sum_sq_output, torch.ones_like(sum_sq_output))
+        assert torch.allclose(
+            sum_sq_output, number_eigenvals * torch.ones_like(sum_sq_output)
+        )
 
 
 def test_masked_basis_are_masked_minimal_basis(
@@ -168,6 +179,7 @@ def test_equivariance_minimal_basis(dataset_options_factory, network_factory):
                 edge_index_interpolated_transition_state
             ),
             node_attr=node_attr,
+            number_eigenvalues=torch.tensor([1]),
         )
 
         return model(data)
@@ -222,6 +234,7 @@ def test_equivariance_full_basis(dataset_options_factory, network_factory):
                 edge_index_interpolated_transition_state
             ),
             node_attr=node_attr,
+            number_eigenvalues=torch.tensor([1]),
         )
 
         return model(data)

training/figure1_pyridene.py
@@ -1,8 +1,8 @@
 from pathlib import Path
+
+import matplotlib.pyplot as plt
 import numpy as np
 from ase import units as ase_units
-import matplotlib.pyplot as plt
-
 from figure1 import get_data
 from instance_mongodb import instance_mongodb_sei
 from plot_params import get_plot_params
@@ -12,13 +12,15 @@ __output_dir__.mkdir(exist_ok=True)
 basis_set = "def2-svp"
 get_plot_params()
 
+
 def get_selected_eigenvalues(eigenvalues):
     """Get the selected eigenvalues."""
     selected_eigenval = eigenvalues[eigenvalues < 0]
     selected_eigenval = np.sort(selected_eigenval)
     selected_eigenval = selected_eigenval[-1]
     selected_eigenval_index = np.where(eigenvalues == selected_eigenval)[0][0]
-    return list(range(selected_eigenval_index-1, selected_eigenval_index+2))
+    return list(range(selected_eigenval_index - 1, selected_eigenval_index + 2))
+
 
 def get_interesting_indices(coeff_matrix):
     """Given a Nx3 matrix, return the row indices (<N) which are interesting.
@@ -26,20 +28,21 @@ def get_interesting_indices(coeff_matrix):
     interesting_indices = np.where(np.any(np.abs(coeff_matrix) > 0.125, axis=1))[0]
     return interesting_indices
 
+
 if __name__ == "__main__":
     db = instance_mongodb_sei(project="mlts")
     collection = db.rotated_pyridene_calculations
     data = get_data(basis_set=basis_set, collection=collection)
     #
     eigenvals = data.eigenvalues[0] * ase_units.Hartree
-    ortho_coeff_matrix = data.ortho_coeff_matrix[0,...].T
+    ortho_coeff_matrix = data.ortho_coeff_matrix[0, ...].T
     selected_eigenvals = get_selected_eigenvalues(eigenvals)
-    truncated_ortho_coeff_matrix = ortho_coeff_matrix[:,selected_eigenvals] 
+    truncated_ortho_coeff_matrix = ortho_coeff_matrix[:, selected_eigenvals]
     interesting_ind = get_interesting_indices(truncated_ortho_coeff_matrix)
     truncated_ortho_coeff_matrix = truncated_ortho_coeff_matrix[interesting_ind]
     #
     fig, ax = plt.subplots(1, 1, figsize=(6, 3.5), constrained_layout=True)
-    plot_kwargs = {"cmap": "coolwarm", "vmin":-1, "vmax":1}
+    plot_kwargs = {"cmap": "coolwarm", "vmin": -1, "vmax": 1}
     cax = ax.imshow(truncated_ortho_coeff_matrix, **plot_kwargs)
     basis_function_orbital = data.basis_functions_orbital
     truncated_basis_functions = basis_function_orbital[interesting_ind]